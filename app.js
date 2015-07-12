af5c55480207be1aed596141959685fcbec2eed6
lipho <lipho@plus20charisma.com>
2015-07-12 15:26:14 -0700

    Add upvote & submit link functions.

--- app.js
+++ app.js
@@ -4,8 +4,28 @@
 var app = angular.module('munkkit', []);
 
 app.controller('MainCtrl', [
-    '$scope',
-    function($scope){
-        $scope.test = 'Hello World!';
-    }
+  '$scope',
+  function($scope) {
+    $scope.test = 'Hello World!';
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 42},
+      {title: 'post 3', upvotes: 534},
+      {title: 'post 4', upvotes: 2},
+      {title: 'post 5', upvotes: 66},
+    ];
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') {return;}
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.link = '';
+      $scope.title = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+  }
 ]);
